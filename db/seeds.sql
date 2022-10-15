ff6ce378b4bc1dd00a087e3a7e4849fcc88c8958
Alex Defrese <aidandefrese@gmail.com>
2022-10-15 16:08:35 -0400

    added functions, tried to fix addEmployee

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,22 @@
-use employees;
+USE employees;
 
 INSERT INTO department 
     (name)
 VALUES
-    ('SALES'),
+    ('Sales'),
     ('IT'),
-    ('LEGAL'),
-    ('ENGINEERING'),
-    ('FINANCE');
+    ('Legal'),
+    ('Engineering'),
+    ('Finance');
 
     INSERT INTO role 
     (title, salary, department_id)
 VALUES
-    ('SALES LEAD', 75000, 1),
-    ('IT SUPPORT', 100000, 2),
-    ('LAWYER', 30000, 3),
-    ('ENGINEER', 150000, 4),
-    ('ACCOUNTANT',51000, 5);
+    ('Sales Lead', 75000, 1),
+    ('IT Support', 100000, 2),
+    ('Lawyer', 30000, 3),
+    ('Engineer', 150000, 4),
+    ('Accountant',51000, 5);
 
     INSERT INTO employee 
     (first_name, last_name, role_id, manager_id)
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { prompt } = require('inquirer');
 const mysql = require('mysql2');
-console.table = require ('console.table');
+require ('console.table');
 
 const db = mysql.createConnection(
     {
@@ -9,9 +9,9 @@ const db = mysql.createConnection(
       user: 'root',
       // MySQL password
       password: 'Harrison1991',
-      database: 'inventory_db'
+      database: 'employees'
     },
-    console.log(`Connected to the inventory_db database.`)
+    console.log(`Connected to the employees database.`)
   ).promise();
   
 
@@ -27,8 +27,28 @@ const db = mysql.createConnection(
                     value: 'VIEW_EMPLOYEES'
                 },
                 {
+                    name: 'Add Employee',
+                    value: 'ADD_EMPLOYEE'
+                },
+                {
+                    name:'Update Employee Role',
+                    value: 'UPDATE_EMPLOYEE'
+                },
+                {
                     name: 'View all Departments',
                     value: 'VIEW_DEPARTMENTS'
+                },
+                {
+                    name: 'Add a Department',
+                    value: 'ADD_DEPARTMENT'
+                },
+                {
+                    name: 'View all Roles',
+                    value: 'VIEW_ROLES'
+                },
+                {
+                    name: 'Add a Role',
+                    value: 'ADD_ROLE'
                 },
                 {
                     name: 'EXIT',
@@ -38,6 +58,7 @@ const db = mysql.createConnection(
         }
     ])
 
+
     switch (choice) {
         case 'VIEW_EMPLOYEES':
             viewEmployees()
@@ -45,22 +66,80 @@ const db = mysql.createConnection(
         case 'VIEW_DEPARTMENTS':
             viewDepartments()
             break;
+        case 'VIEW_ROLES':
+            viewRoles()
+            break;
+        case 'ADD_EMPLOYEE':
+            addEmployee()
+            break;
+        case 'UPDATE_EMPLOYEE':
+            updateEmployee()
+            break;
+        case 'ADD_DEPARTMENT':
+            addDepartment()
+            break;
+        case 'ADD_ROLE':
+            addRole();
         case 'EXIT':
             process.exit();
         default:
             process.exit();
-    };
+    }
+};
 
-    const viewEmployees = async() => {
-        const employeeData = await db.query("SELECT * FROM employee")
-        console.log(employeeData);
+    const viewEmployees = async () => {
+        const [employeeData] = await db.query(`SELECT * FROM employee`)
+        console.table(employeeData);
         mainMenu();
     };
 
-    const viewDepartments = () => {
+    const viewDepartments = async () => {
+        const [departmentData] = await db.query(`SELECT  * FROM department`)
+        console.table(departmentData);
+        mainMenu();
+    };
 
+    const viewRoles = async () => {
+        const [roleData] = await db.query(`SELECT * FROM role`)
+        console.table(roleData);
+        mainMenu();
+    };
+
+    const addEmployee = () => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id,manager_id) VALUES (?, ?, ?, ?)`;
+        const params = async () => { 
+            prompt ([
+            {
+                type: 'input',
+                name:'firstName',
+                message:"What is the employee's first name?"
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message:"What is the employee's last name?"
+            },
+            {
+                type: 'list',
+                name: 'roleType',
+                choices: [
+                    'Sales Lead',
+                    'IT Support',
+                    'Lawyer',
+                    'Engineer',
+                    'Accountant'
+                ]
+            }
+        ])
     }
+        db.query(sql, params, (result));
+        console.log('employee added');
+        mainMenu();
 
-  }
+    };
+   
+    
+
+
 
   mainMenu();
